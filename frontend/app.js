c8eb3bd0a228a037fb26363b921bd582ef7de4df
Erin Rutan <erin.rutan.17@cnu.edu>
2021-02-23 22:15:40 -0500

    set up

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,27 @@
-alert('hello world!');
+var app = new Vue({
+    el: '#app', // corresponds with div id in html file
+    data: {
+        product: 'Socks',
+        image: '../frontend/greensocks.jpg',
+        inventory: 100,
+        details: ["80% cotton","20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: '../frontend/greensocks.jpg'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: '../frontend/row.jpeg'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1
+        }
+    }
+})
