a523fb46eb3e69e978467c91388ac90dfe4c755e
Erin Rutan <erin.rutan.17@cnu.edu>
2021-03-13 15:36:40 -0500

    added settings page

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el: '#app', // corresponds with div id in html file
     data: {
-        image: '../frontend/row.jpeg'
+        image: '../frontend/row2.jpeg'
     },
     methods: {
         goToSchedule: function () {
---
+++ frontend/settings.js
@@ -0,0 +1,6 @@
+var app = new Vue({
+    el: '#settings',
+    data: {
+        image: '../frontend/back_button.png'
+    }    
+})
