b8bee6c4ac13f9f8d8796671ae1a9805e177d5cc
Erin Rutan <erin.rutan.17@cnu.edu>
2021-04-17 17:50:43 -0400

    trying something different part 2

--- frontend/settings.php
+++ frontend/settings.php
@@ -166,8 +166,7 @@
         <div class="textarea">
             <div class="center">
                 <form method="POST">
-                <?php echo $membername;?>
-                    Name: <input type="text" name="membername" autocomplete="off" value=<?php echo $membername;?>>
+                    Name: <input type="text" name="membername" autocomplete="off" value='<?php echo $membername;?>'>
                     <br/>
                     Phone Number: <input type="text" name="memberphoneno" value=<?php echo $memberphoneno;?>>
                     <br/>
