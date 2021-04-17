293992470c371df60d737eea14aca8f732814a9a
Erin Rutan <erin.rutan.17@cnu.edu>
2021-04-17 18:50:54 -0400

    trying something different to get alignment

--- frontend/settings.php
+++ frontend/settings.php
@@ -231,7 +231,9 @@
                     </p>
                     <br/><br/>
                     <div class="box">
+                    <div class='center'>
                     <input type="submit" name="save" class="buttonPop" href="#popup1" value="Save Changes">
+                    </div>
                 </form>
             </div>
             <br/>
