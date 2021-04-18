6c7704d5799060af2bb379ebb151845a1e261c15
Erin Rutan <erin.rutan.17@cnu.edu>
2021-04-18 19:56:15 -0400

    getting rid of connected message

--- frontend/db.php
+++ frontend/db.php
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-echo 'Connected successfully';
+// echo 'Connected successfully';
 
 mysqli_close($conn);
 
