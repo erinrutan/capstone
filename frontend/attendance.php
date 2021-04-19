b8a6a1fe51472256388d214727c1464ca686bfe3
Erin Rutan <erin.rutan.17@cnu.edu>
2021-04-18 21:28:40 -0400

    responsive web design?

--- frontend/attendance.php
+++ frontend/attendance.php
@@ -50,6 +50,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>CNU Rowing Club</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
--- frontend/schedule.php
+++ frontend/schedule.php
@@ -34,6 +34,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <!-- things for calendar -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
