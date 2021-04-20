063cd6d9efaeba979511743515aecb8cae97748b
Erin Rutan <erin.rutan.17@cnu.edu>
2021-04-20 14:53:15 -0400

    get event time

--- frontend/attendance.php
+++ frontend/attendance.php
@@ -21,14 +21,15 @@
     $getEvent = mysqli_fetch_assoc(mysqli_query( $conn, $sql));    
     $currentEventID = $getEvent['eventid'];
     $currentEventName = $getEvent['eventname'];
-    $currentEventTime = mysqli_fetch_assoc(mysqli_query($conn, "SELECT substring(eventdate,12,5) FROM event WHERE eventid = '$currentEventID';"));
+    $getTime = mysqli_fetch_assoc(mysqli_query($conn, "SELECT substring(eventdate,12,5) as 'time' FROM event WHERE eventid = '$currentEventID';"));
+    $currentEventTime = $getTime['time'];
     echo " Event Name: ";
     echo $currentEventName;
     echo " Event ID: ";
     echo $currentEventID;
     echo " Event Time: ";
     echo $currentEventTime;
-    
+
     $isEventToday = False;
     if ($getEvent != NULL) {
         $isEventToday = True;
